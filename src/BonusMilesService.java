be6771f7b558e0b8ce6d049670069b81918493fa
vugar350 <Vugar350@mail.ru>
2021-11-30 19:07:07 +0300

    initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,7 @@
+public class BonusMilesService {
+    public  int calculate(int cost) {
+        int Price=10000;
+        int miles=Price/20;
+
+        return miles;}
+    }
--- src/main.java
+++ src/main.java
@@ -1,8 +1,8 @@
 public class main {
     public static void main(String[] args) {
-        int ticketPrice=10000;
-        int bonus=ticketPrice/20;
-        System.out.println(bonus);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles= (int) service.calculate(price);
+        System.out.println(miles);
     }
 }
